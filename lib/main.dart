31f267f5f3447e01b5d86d284a44341b97f4dca4
DevTranVanNguyen <nobitavvvvvv@gmail.com>
2021-11-16 00:04:01 +0700

    start building app

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const Quizzler());
+
+class Quizzler extends StatelessWidget {
+  const Quizzler({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: const SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
